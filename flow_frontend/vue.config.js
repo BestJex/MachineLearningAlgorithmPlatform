ee63c67ca8e56feab1111b6481aeeee5dddde5a0
Alex <alex18812649207@gmail.com>
2020-07-08 20:38:09 +0800

    配置vue.config——2020-7-8

--- flow_frontend/vue.config.js
+++ flow_frontend/vue.config.js
@@ -37,7 +37,7 @@ module.exports = {
         },
         proxy: {
             "/api": {
-                target:'http://39.105.21.62/flow/api',
+                target: 'http://39.105.21.62/flow/api',
                 changeOrigin: true,
                 pathRewrite: {
                     '^/api': ''
@@ -46,10 +46,6 @@ module.exports = {
         }
     },
     configureWebpack: {
-
-        // provide the app's title in webpack's name field, so that
-        // it can be accessed in index.html to inject the correct title.
-        // name: name,
         resolve: {
             alias: {
                 '@': resolve('src')
@@ -58,93 +54,18 @@ module.exports = {
         output: {
             libraryExport: 'default'
         }
-        // externals: {
-        //   'vue': 'Vue',
-        //   'element-ui': 'ELEMENT',
-        // },
     },
     chainWebpack(config) {
         config.plugins.delete('preload') // TODO: need test
         config.plugins.delete('prefetch') // TODO: need test
-        // config
-        //   // 插件名
-        //   .plugin('extract-css')
-        //   // 修改规则
-        //   .tap(args => {
-        //     args[0].filename = 'css/styles.css'
-        //     args[0].chunkFilename = 'css/[name].css'
-        //     return args
-        //   })
-        // set svg-sprite-loader
-        config.module
-            .rule('svg')
-            .exclude.add(resolve('src/icons'))
-            .end()
-        config.module
-            .rule('icons')
-            .test(/\.svg$/)
-            .include.add(resolve('src/icons'))
-            .end()
-            .use('svg-sprite-loader')
-            .loader('svg-sprite-loader')
-            .options({
+        config.module.rule('svg').exclude.add(resolve('src/icons')).end()
+        config.module.rule('icons').test(/\.svg$/).include.add(resolve('src/icons')).end().use('svg-sprite-loader').loader('svg-sprite-loader').options({
                 symbolId: 'icon-[name]'
-            })
-            .end()
-        // set preserveWhitespace
-        config.module
-            .rule('vue')
-            .use('vue-loader')
-            .loader('vue-loader')
-            .tap(options => {
+            }).end()
+        config.module.rule('vue').use('vue-loader').loader('vue-loader').tap(options => {
                 options.compilerOptions.preserveWhitespace = true
                 return options
-            })
-            .end()
-
-        config
-            // https://webpack.js.org/configuration/devtool/#development
-            .when(process.env.NODE_ENV === 'development',
-                config => config.devtool('cheap-source-map')
-            )
-
-        // config
-        //   .when(process.env.NODE_ENV !== 'development',
-        //     config => {
-        //       config
-        //         .plugin('ScriptExtHtmlWebpackPlugin')
-        //         .after('html')
-        //         .use('script-ext-html-webpack-plugin', [{
-        //           // `runtime` must same as runtimeChunk name. default is `runtime`
-        //           inline: /runtime\..*\.js$/
-        //         }])
-        //         .end()
-        //       config
-        //         .optimization.splitChunks({
-        //           chunks: 'all',
-        //           cacheGroups: {
-        //             libs: {
-        //               name: 'chunk-libs',
-        //               test: /[\\/]node_modules[\\/]/,
-        //               priority: 10,
-        //               chunks: 'initial' // only package third parties that are initially dependent
-        //             },
-        //             elementUI: {
-        //               name: 'chunk-elementUI', // split elementUI into a single package
-        //               priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-        //               test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
-        //             },
-        //             commons: {
-        //               name: 'chunk-commons',
-        //               test: resolve('src/components'), // can customize your rules
-        //               minChunks: 3, //  minimum common number
-        //               priority: 5,
-        //               reuseExistingChunk: true
-        //             }
-        //           }
-        //         })
-        //       config.optimization.runtimeChunk('single')
-        //     }
-        //   )
+            }).end()
+        config.when(process.env.NODE_ENV === 'development', config => config.devtool('cheap-source-map'))
     }
 }
---
+++ frontend/vue.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+
+function resolve(dir) {
+    return path.join(__dirname, dir);
+}
+
+module.exports = {
+    publicPath: '/flow/',
+    outputDir: 'flow',      // build 生成文件的目录，若已存在则会被先清除，传入 --no-clean 可关闭该行为
+    assetsDir: 'assets',    // 放置生成的静态资源 (js、css、img、fonts) 的目录，相对于 outputDir
+    lintOnSave: process.env.NODE_ENV !== 'production',  // 生产构建时禁用 eslint-loader
+    crossorigin: 'anonymous',   // anonymous 不会通过 cookies，客户端 SSL 证书或 HTTP 认证交换用户凭据
+    productionSourceMap: false, // 生产环境的 source map，可以将其设置为 false 以加速生产环境构建
+    // 配置 webpack-dev-server
+    devServer: {
+        port: 9528,
+        open: true,
+        overlay: {
+            warnings: false,
+            errors: true
+        },
+        proxy: null
+    },
+    // webpack 配置
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@': resolve('src'),
+            }
+        },
+        output: {
+            libraryExport: 'default'
+        },
+    },
+    // Vue CLI 内部的 webpack 配置是通过 webpack-chain 维护
+    chainWebpack: config => {
+        config.plugins.delete('preload') // TODO: need test
+        config.plugins.delete('prefetch') // TODO: need test
+        config.module.rule('svg').exclude.add(resolve('src/icons')).end()
+        config.module.rule('icons').test(/\.svg$/).include.add(resolve('src/icons')).end().use('svg-sprite-loader').loader('svg-sprite-loader').options({
+            symbolId: 'icon-[name]'
+        }).end()
+        config.module.rule('vue').use('vue-loader').loader('vue-loader').tap(options => {
+            options.compilerOptions.preserveWhitespace = true
+            return options
+        }).end()
+        config.when(process.env.NODE_ENV === 'development', config => config.devtool('cheap-source-map'))
+    }
+}
